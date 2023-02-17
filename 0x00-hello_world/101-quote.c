1c0a4529730d9fda23639f32969b7ec13a11becc
Gabriel <ochibaedgar@gmail.com>
2023-02-16 17:31:10 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line to a standard error
+ * Return: 1(Success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
