30b97312c0fcafc744192c91b9a82391814615e5
Gabriel <ochibaedgar@gmail.com>
2023-02-23 13:30:24 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -14,22 +14,26 @@ int main(void)
 
 	n = 612852475143;
 	max = -1;
+
 	while (n % 2 == 0)
 	{
-		max = 2;
-		n /= 2;
+	max = 2;
+	n /= 2;
 	}
+
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 	while (n % i == 0)
 	{
-		max = i;
-		n = n / i;
+	max = i;
+	n = n / i;
 	}
 	}
+
 	if (n > 2)
-		max = n;
-	}
+	max = n;
+
 	printf("%ld\n", max);
+
 	return (0);
 }
