800fce99c380b2849845fcd1284dfb26085491ce
Gabriel <ochibaedgar@gmail.com>
2023-02-23 13:09:22 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,10 +11,11 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 	for (j = 0; j < 15; j++)
-	{
-	if (j >= 10)
-		_putchar(j / 10 + '0');
-		_putchar(j % 10 + '0');
+		{
+		if (j >= 10)
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
 	}
 	_putchar('\n');
 }
