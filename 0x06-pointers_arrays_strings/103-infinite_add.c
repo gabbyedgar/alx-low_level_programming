9dcc18d2a0fbb14d2eb27390f7a4be20b600b0d2
Gabriel <ochibaedgar@gmail.com>
2023-03-02 16:15:33 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,31 +1,4 @@
-#include "main.h"
-
-/**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
- */
-
-void rev_string(char *n)
-{
-	int i = 0;
-	int j = 0;
-	char temp;
-
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
-
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
-}
-
+include "main.h"
 /**
  * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
@@ -74,4 +47,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		return (0);
 	*(r + digits) = '\0';
 	rev_string(r);
+	return (r);
 }
