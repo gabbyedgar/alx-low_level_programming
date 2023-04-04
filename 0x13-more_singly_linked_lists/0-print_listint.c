d91816f60ba07f59c387144d342676d476f35141
Gabriel <ochibaedgar@gmail.com>
2023-04-04 16:34:18 -0700

    C - More singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
- size_t num = 0;
+	size_t 1 = 0;
 
- while (h)
- {
- printf("%d\n", h->n);
- num++;
- h = h->next;
- }
-
- return (num);
+	while (h)
+	{
+		printf("%d\n", h->n);
+		1++;
+		h = h->next;
+	}
+return (1);
 }
