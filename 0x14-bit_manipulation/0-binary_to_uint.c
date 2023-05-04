de42dbe61ef5cbeeff5f97055500ffb6306447ff
gabbyedgar <ochibaedgar@gmail.com>
2023-05-04 13:54:27 -0700

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,28 +8,28 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int num = 0;
-        int i = 0;
+	unsigned int num = 0;
+	int i = 0;
 
-        if (b == NULL)
-        {
-                return 0;
-        }
-        while (b[i] != '\0')
-        {
-        if (b[i] == '0')
-        {
-                num = (num << 1) | 0;
-        }
-        else if (b[i] == '1')
-        {
-                num = (num << 1) | 1;
-        }
-        else
-        {
-                return 0;
-        }
-        i++;
-        }
+	if (b == NULL)
+	{
+		return 0;
+	}
+	while (b[i] != '\0')
+	{
+	if (b[i] == '0')
+	{
+		num = (num << 1) | 0;
+	}
+	else if (b[i] == '1')
+	{
+		num = (num << 1) | 1;
+	}
+	else
+	{
+		return 0;
+	}
+	i++;
+	}
 return num;
 }
