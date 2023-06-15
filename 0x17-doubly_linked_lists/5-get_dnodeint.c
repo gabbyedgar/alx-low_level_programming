67a49eaf9bfd46ba83eb5082499321b27cb1b1aa
gabbyedgar <ochibaedgar@gmail.com>
2023-06-15 14:50:57 -0700

    doublylinked

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Locates a node in a dlistint_t list.
+ * @head: The head of the dlistint_t list.
+ * @index: The node to locate.
+ *
+ * Return: If the node does not exist - NULL.
+ *         Otherwise - the address of the located node.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (head != NULL)
+	{
+		if (index == count)
+		{
+			return (head);
+		}
+		count++;
+		head = head->next;
+	}
+	return (NULL);
+}
